3663fc92dfbe2b0c7a1a0c315b675e9bbdd881df
Ash Priom <sa.priom@gmail.com>
2016-07-30 02:29:48 -0400

    new port definition

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(8080,function(){
-  console.log("Live at Port 8080");
+//app.listen(8080,function(){
+//  console.log("Live at Port 8080");
+//});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
